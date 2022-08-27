fdde3d4993c08334b0e219f7625a6d89a1ceaeb8
kotoko <kotoko@users.noreply.github.com>
2022-08-27 14:45:42 +0200

    Fix 'pre_build_script'

--- files/register-gitlab-runner.sh
+++ files/register-gitlab-runner.sh
@@ -44,7 +44,7 @@ gitlab-runner register \
 	--builds-dir "$BUILDS_DIR" \
 	--cache-dir "$CACHE_DIR" \
 	--pre-clone-script '/usr/bin/bash-static /root/fix-git-dubious-ownership.sh' \
-	--pre-build-script '/usr/bin/bash-static /usr/bin/neofetch --backend off \n/usr/bin/bash-static /root/print-free-space.sh' \
+	--pre-build-script '/usr/bin/bash-static -c "/usr/bin/neofetch --backend off ; /root/print-free-space.sh"' \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
 	--env "$FF_SCRIPT_SECTIONS"
@@ -66,7 +66,7 @@ gitlab-runner register \
 	--docker-volumes '/root/fix-git-dubious-ownership.sh:/host/fix-git-dubious-ownership.sh:ro' \
 	--docker-volumes '/root/print-free-space.sh:/host/print-free-space.sh:ro' \
 	--pre-clone-script '/host/bash-static /host/fix-git-dubious-ownership.sh' \
-	--pre-build-script '/host/bash-static /host/neofetch --backend off \n/host/bash-static /host/fix-git-dubious-ownership.sh \n/host/bash-static /host/print-free-space.sh' \
+	--pre-build-script '/host/bash-static -c "/host/neofetch --backend off ; /host/fix-git-dubious-ownership.sh ; /host/print-free-space.sh"' \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
 	--env "$FF_SCRIPT_SECTIONS"
@@ -89,7 +89,7 @@ gitlab-runner register \
 	--docker-volumes '/root/fix-git-dubious-ownership.sh:/host/fix-git-dubious-ownership.sh:ro' \
 	--docker-volumes '/root/print-free-space.sh:/host/print-free-space.sh:ro' \
 	--pre-clone-script '/host/bash-static /host/fix-git-dubious-ownership.sh' \
-	--pre-build-script '/host/bash-static /host/neofetch --backend off \n/host/bash-static /host/fix-git-dubious-ownership.sh \n/host/bash-static /host/print-free-space.sh' \
+	--pre-build-script '/host/bash-static -c "/host/neofetch --backend off ; /host/fix-git-dubious-ownership.sh ; /host/print-free-space.sh"' \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
 	--env "$FF_SCRIPT_SECTIONS"
