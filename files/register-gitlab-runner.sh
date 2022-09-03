7b3e2eb6abef966d743fa022ad2d88b229d4bf91
kotoko <kotoko@users.noreply.github.com>
2022-09-03 09:53:24 +0200

    Add FF_USE_IMPROVED_URL_MASKING

--- files/register-gitlab-runner.sh
+++ files/register-gitlab-runner.sh
@@ -5,10 +5,12 @@ REGISTRATION_TOKEN=''
 URL='https://gitlab.com'
 BUILDS_DIR='/gitlab-runner/builds'
 CACHE_DIR='/gitlab-runner/cache'
+
 FF_ENABLE_JOB_CLEANUP='FF_ENABLE_JOB_CLEANUP=1'
 FF_SCRIPT_SECTIONS='FF_SCRIPT_SECTIONS=1'
 FF_USE_FASTZIP='FF_USE_FASTZIP=1'
 FF_NETWORK_PER_BUILD='FF_NETWORK_PER_BUILD=1'
+FF_USE_IMPROVED_URL_MASKING='FF_USE_IMPROVED_URL_MASKING=1'
 
 
 if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
@@ -48,7 +50,8 @@ gitlab-runner register \
 	--pre-build-script '/usr/bin/bash-static -c "/usr/bin/bash-static /usr/bin/neofetch --backend off ; /usr/bin/bash-static /root/print-free-space.sh"' \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
-	--env "$FF_SCRIPT_SECTIONS"
+	--env "$FF_SCRIPT_SECTIONS" \
+	--env "$FF_USE_IMPROVED_URL_MASKING"
 
 # docker
 gitlab-runner register \
@@ -71,7 +74,8 @@ gitlab-runner register \
 	--env "$FF_NETWORK_PER_BUILD" \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
-	--env "$FF_SCRIPT_SECTIONS"
+	--env "$FF_SCRIPT_SECTIONS" \
+	--env "$FF_USE_IMPROVED_URL_MASKING"
 
 # docker privileged
 gitlab-runner register \
@@ -95,4 +99,5 @@ gitlab-runner register \
 	--env "$FF_NETWORK_PER_BUILD" \
 	--env "$FF_ENABLE_JOB_CLEANUP" \
 	--env "$FF_USE_FASTZIP" \
-	--env "$FF_SCRIPT_SECTIONS"
+	--env "$FF_SCRIPT_SECTIONS" \
+	--env "$FF_USE_IMPROVED_URL_MASKING"
