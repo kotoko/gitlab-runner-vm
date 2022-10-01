e74ce3e7654375a1be2efbc1e2fb34ec0fec8e8b
kotoko <kotoko@users.noreply.github.com>
2022-10-01 22:24:15 +0200

    Update scipts

--- files/fix-git-dubious-ownership.sh
+++ files/fix-git-dubious-ownership.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+set -u
+
+echo "$0"
 
 if command -v git >/dev/null 2>/dev/null; then
 	echo 'git config --global safe.directory "*"'
--- files/print-free-space.sh
+++ files/print-free-space.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+set -u
+
+echo "$0"
 
 if command -v df >/dev/null 2>/dev/null; then
 	echo "df -h '${CI_PROJECT_DIR}'"
